3536c54327ece72874855f2593b41eff5e383b85
mnom <mouhamedsoumare70@gmail.com>
2024-05-27 12:27:16 +0000

    initializing the project

---
+++ trafic_simulation/target/debug/deps/trafic_simulation-332b741040bb5573.d
@@ -0,0 +1,5 @@
+/home/cptn3mo/Desktop/zone/smart-road/trafic_simulation/target/debug/deps/libtrafic_simulation-332b741040bb5573.rmeta: src/main.rs
+
+/home/cptn3mo/Desktop/zone/smart-road/trafic_simulation/target/debug/deps/trafic_simulation-332b741040bb5573.d: src/main.rs
+
+src/main.rs:
---
+++ trafic_simulation/target/debug/deps/trafic_simulation-e89c19df9f49ef7b.d
@@ -0,0 +1,5 @@
+/home/cptn3mo/Desktop/zone/smart-road/trafic_simulation/target/debug/deps/libtrafic_simulation-e89c19df9f49ef7b.rmeta: src/main.rs
+
+/home/cptn3mo/Desktop/zone/smart-road/trafic_simulation/target/debug/deps/trafic_simulation-e89c19df9f49ef7b.d: src/main.rs
+
+src/main.rs:
